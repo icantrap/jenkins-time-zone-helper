d2b43eec294730889986dc83583cbf4165af0480
icantrap <icantrap08@gmail.com>
2019-01-22 10:34:30 -0500

    Source maps and mode.
    - set up source maps for non-prod.
    - set up mode.

--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,25 @@ const webpack = require('webpack');
 const WebpackNotifierPlugin = require('webpack-notifier');
 
 const config = {
-    entry: {
-        content: ['./src/content/index.js'],
-        background: ['./src/background/index.js']
-    },
-    output: {
-        path: path.join(__dirname, "./ext"),
-        filename: 'bundle/[name].js'
-    },
-    plugins: [
-        new WebpackNotifierPlugin(),
-        new webpack.ProvidePlugin({$: 'jquery'})
-    ]
+  entry: {
+    content: ['./src/content/index.js']
+  },
+  output: {
+    path: path.join(__dirname, "./ext"),
+    filename: 'bundle/[name].js'
+  },
+  plugins: [
+    new WebpackNotifierPlugin(),
+    new webpack.ProvidePlugin({$: 'jquery'})
+  ]
 };
+
+if ('production' === process.env.NODE_ENV) {
+  config.mode = 'production';
+}
+else {
+  config.mode = 'development';
+  config.devtool = 'source-map';
+}
 
 module.exports = config;
